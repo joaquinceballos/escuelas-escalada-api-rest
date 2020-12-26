f4c527ef000f25bb9f18ec1156a4d3ed0c5a0827
Joaquín Ceballos Gómez <uo250687@uniovi.es>
2020-12-26 18:12:36 +0100

    entorno de test

--- src/test/java/es/uniovi/escuelas/ApiEscuelasDeEscaladaApplicationTests.java
+++ src/test/java/es/uniovi/escuelas/ApiEscuelasDeEscaladaApplicationTests.java
@@ -1,13 +1,21 @@
 package es.uniovi.escuelas;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest()
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.HSQLDB)
+@ActiveProfiles("test")
 class ApiEscuelasDeEscaladaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() {		
+		assertTrue(true);
 	}
 
 }
