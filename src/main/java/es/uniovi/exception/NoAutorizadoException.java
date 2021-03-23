d3ea8b299effa57da87422d3b1722a983d990c0a
Joaquín Ceballos Gómez <uo250687@uniovi.es>
2021-03-23 21:35:43 +0100

    servicio de privilegios
    Checka los privilegios del usuario autenticado

--- src/main/java/es/uniovi/exception/NoAutorizadoException.java
+++ src/main/java/es/uniovi/exception/NoAutorizadoException.java
@@ -2,9 +2,14 @@ package es.uniovi.exception;
 
 public class NoAutorizadoException extends ServiceException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1016515840504546650L;
+
+	public NoAutorizadoException() {
+		super();
+	}
+
+	public NoAutorizadoException(String string) {
+		super(string);
+	}
 
 }
--- src/main/java/es/uniovi/repository/RolRepository.java
+++ src/main/java/es/uniovi/repository/RolRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
+import es.uniovi.domain.NombreRol;
 import es.uniovi.domain.Rol;
 
 public interface RolRepository extends CrudRepository<Rol, Long> {
@@ -14,6 +15,6 @@ public interface RolRepository extends CrudRepository<Rol, Long> {
 	 * @param nombre El nombre del rol
 	 * @return El Rol
 	 */
-	Optional<Rol> findByNombre(String nombre);
+	Optional<Rol> findByNombre(NombreRol nombre);
 
 }
